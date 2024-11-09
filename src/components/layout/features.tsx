f6543d6e6ae87e2abe4c003fc3e8c8016d92cb34
bobgodh <despairlord15@gmail.com>
2024-11-10 01:26:24 +0530

    fix

--- src/components/layout/features.tsx
+++ src/components/layout/features.tsx
@@ -1,5 +1,6 @@
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import { MedalIcon, MapIcon, PlaneIcon, GiftIcon } from "lucide-react";
+import { JSX } from "react";
 
 interface FeatureProps {
   icon: JSX.Element;

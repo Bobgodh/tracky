e410cec7b174acd9b42bd6e54bd98369fad66809
bobgodh <despairlord15@gmail.com>
2024-11-09 23:09:38 +0530

    revert change

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,12 +5,15 @@ const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)'])
 const isOnboardingRoute = createRouteMatcher(['/onboarding'])
 
 export default clerkMiddleware(async (auth, req) => {
-	const { userId, redirectToSignIn } = await auth()
+	const { userId, sessionClaims, redirectToSignIn } = await auth()
 
 	if (userId && isOnboardingRoute(req)) return NextResponse.next()
 
 	if (!userId && !isPublicRoute(req))
 		return redirectToSignIn({ returnBackUrl: req.url })
+
+	if (userId && !sessionClaims?.metadata?.onboardingCompleted)
+		return NextResponse.redirect(new URL('/onboarding', req.url))
 
 	if (userId && !isPublicRoute(req)) return NextResponse.next()
 })

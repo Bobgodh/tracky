48055bc4ec8b4035f5e8e7ed6bf202d907c961ed
bobgodh <despairlord15@gmail.com>
2024-11-09 22:47:00 +0530

    e

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,22 +1,3 @@
-import { createRouteMatcher, clerkMiddleware } from '@clerk/nextjs/server'
-import { NextResponse } from 'next/server'
-
-const isPublicRoute = createRouteMatcher(['/', '/sign-in(.*)', '/sign-up(.*)'])
-const isOnboardingRoute = createRouteMatcher(['/onboarding'])
-
-export default clerkMiddleware(async (auth, req) => {
-	const { userId, sessionClaims, redirectToSignIn } = await auth()
-
-	if (userId && isOnboardingRoute(req)) return NextResponse.next()
-
-	if (!userId && !isPublicRoute(req))
-		return redirectToSignIn({ returnBackUrl: req.url })
-
-	if (userId && !sessionClaims?.metadata?.onboardingCompleted)
-		return NextResponse.redirect(new URL('/onboarding', req.url))
-
-	if (userId && !isPublicRoute(req)) return NextResponse.next()
-})
 
 export const config = {
 	matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)']
